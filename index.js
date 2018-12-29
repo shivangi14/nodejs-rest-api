1486ce118aeadf3624c2ab03c6534aeadb53d0a3
shivangi14 <agshivangi14@gmail.com>
2018-12-30 00:59:05 +0530

    Simple GET call added

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+// call the packages we need
+const express = require('express');
+//const bodyParser = require('body-parser');
+const app = express();
+
+//app's config
+//app.use(bodyParser.json()); // support json encoded bodies
+//app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+const server = app.listen(process.env.PORT || 5000, () => {
+	console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+//Simple get call
+app.get('/', (req, res) => {
+	res.send('Hello, This is simple Get API call')
+});
