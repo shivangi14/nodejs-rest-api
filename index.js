cfd0a784457e4d1d5bfedc63b0acbf36584b0744
shivangi14 <agshivangi14@gmail.com>
2018-12-31 01:34:07 +0530

    updated as per API V2

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 // call the packages we need
+const {WebhookClient} = require('dialogflow-fulfillment');
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 
 //app's config
-//app.use(bodyParser.json()); // support json encoded bodies
-//app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 const server = app.listen(process.env.PORT || 5000, () => {
 	console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
@@ -14,4 +15,12 @@ const server = app.listen(process.env.PORT || 5000, () => {
 //Simple get call
 app.get('/', (req, res) => {
 	res.send('Hello, This is simple Get API call')
+});
+
+// Triggered by a POST to /webhook
+app.post('/webhook', (req, res) => {
+	var data = req.body;
+	res.json({
+	    displayText: 'Hi from post'
+	    });
 });
